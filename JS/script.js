59ab391bbb4cd718c3f5669bdadcca3d238123b4
kamiye <kamiye31@gmail.com>
2024-08-20 02:33:38 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+ // Toggle the mobile menu visibility
+ document.getElementById('toggleMenu').addEventListener('click', function() {
+    document.getElementById('mobileMenu').classList.add('active');
+});
+
+// Close the mobile menu
+document.getElementById('closeMenu').addEventListener('click', function() {
+    document.getElementById('mobileMenu').classList.remove('active');
+});
