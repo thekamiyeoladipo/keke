80a393757a5d92b1a3b6312a06cd6322c9ca30a2
kamiye <kamiye31@gmail.com>
2024-08-20 02:58:22 +0100

    new comment js, p html

--- JS/script.js
+++ JS/script.js
@@ -8,5 +8,5 @@ document.getElementById('closeMenu').addEventListener('click', function() {
     document.getElementById('mobileMenu').classList.remove('active');
 });
 
-// this is a comment i want to use to push changes to github
+// this is a comment i want to use to push the first set of  changes to github
 console.log("Publish to github");
