1c71e1859a02565910fc6b94f9c9fad157aa1240
kamiye <kamiye31@gmail.com>
2024-08-20 02:52:46 +0100

    additional section

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,6 @@
 document.getElementById('closeMenu').addEventListener('click', function() {
     document.getElementById('mobileMenu').classList.remove('active');
 });
+
+// this is a comment i want to use to push changes to github
+console.log("Publish to github");
